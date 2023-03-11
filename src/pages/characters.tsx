f8e1b706612a37ab7344bb8b34b26f3a959fc885
Louis Sanna <louis.sanna@ekimetrics.com>
2023-03-11 19:39:52 +0100

    e

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,5 +1,8 @@
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { api } from "~/utils/api";
+import Button from "./components/Button";
 import Layout from "./components/Layout";
 
 const Characters: NextPage = () => {
@@ -12,9 +15,85 @@ const Characters: NextPage = () => {
 
   return (
     <Layout>
-      <div className="flex w-full">Hello Characters!</div>
+      <div className="flex flex-col items-center gap-2">
+          <div className="pt-6">
+            <CreateCharacterForm />
+          </div>
+        </div>
     </Layout>
   );
 };
 
 export default Characters;
+
+
+const CreateCharacterForm: React.FC = () => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const { status } = useSession();
+
+  const utils = api.useContext();
+  const createCharacter = api.character.create.useMutation({
+    onMutate: async (newCharacterElement) => {
+      await utils.character.getAll.cancel();
+      const createdAt = new Date();
+      const newCharacter = {
+        name: newCharacterElement.name,
+        data: { message: newCharacterElement.message },
+        createdAt,
+        updatedAt: createdAt,
+        id: "",
+        userId: "",
+      };
+      utils.character.getAll.setData(undefined, (prevCharacters) => {
+        if (prevCharacters) {
+          return [...prevCharacters, newCharacter];
+        } else {
+          return [newCharacter];
+        }
+      });
+    },
+    onSettled: async () => {
+      await utils.character.getAll.invalidate();
+    },
+  });
+
+  if (status !== "authenticated") return null;
+
+  return (
+    <form
+      className="flex flex-col gap-2 w-96"
+      onSubmit={(event) => {
+        event.preventDefault();
+        createCharacter.mutate({ name, message });
+        setName("");
+        setMessage("");
+      }}
+    >
+      <input
+        type="text"
+        className="rounded-md border-2 border-zinc-800 px-4 py-2 focus:outline-none"
+        placeholder="Your Character Name..."
+        minLength={2}
+        maxLength={100}
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        type="text"
+        className="rounded-md border-2 border-zinc-800 px-4 py-2 focus:outline-none"
+        placeholder="Your Character Message..."
+        minLength={2}
+        maxLength={100}
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+      />
+      <Button
+        type="submit"
+        className="rounded-md bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
+      >
+        Create
+      </Button>
+    </form>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,6 @@ const Home: NextPage = () => {
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           <span className="text-[hsl(280,100%,70%)]">AI</span> for NPC
         </h1>
-        <div className="flex flex-col items-center gap-2">
-          <div className="pt-6">
-            <CreateCharacterForm />
-          </div>
-        </div>
       </div>
     </Layout>
   );
@@ -27,74 +22,3 @@ const Home: NextPage = () => {
 
 export default Home;
 
-
-const CreateCharacterForm: React.FC = () => {
-  const [name, setName] = useState("");
-  const [message, setMessage] = useState("");
-  const { status } = useSession();
-
-  const utils = api.useContext();
-  const createCharacter = api.character.create.useMutation({
-    onMutate: async (newCharacterElement) => {
-      await utils.character.getAll.cancel();
-      const createdAt = new Date();
-      const newCharacter = {
-        name: newCharacterElement.name,
-        data: { message: newCharacterElement.message },
-        createdAt,
-        updatedAt: createdAt,
-        id: "",
-        userId: "",
-      };
-      utils.character.getAll.setData(undefined, (prevCharacters) => {
-        if (prevCharacters) {
-          return [...prevCharacters, newCharacter];
-        } else {
-          return [newCharacter];
-        }
-      });
-    },
-    onSettled: async () => {
-      await utils.character.getAll.invalidate();
-    },
-  });
-
-  if (status !== "authenticated") return null;
-
-  return (
-    <form
-      className="flex gap-2"
-      onSubmit={(event) => {
-        event.preventDefault();
-        createCharacter.mutate({ name, message });
-        setName("");
-        setMessage("");
-      }}
-    >
-      <input
-        type="text"
-        className="rounded-md border-2 border-zinc-800 px-4 py-2 focus:outline-none"
-        placeholder="Your Character Name..."
-        minLength={2}
-        maxLength={100}
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-      />
-      <input
-        type="text"
-        className="rounded-md border-2 border-zinc-800 px-4 py-2 focus:outline-none"
-        placeholder="Your Character Message..."
-        minLength={2}
-        maxLength={100}
-        value={message}
-        onChange={(event) => setMessage(event.target.value)}
-      />
-      <Button
-        type="submit"
-        className="rounded-md bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
-      >
-        Create
-      </Button>
-    </form>
-  );
-};
