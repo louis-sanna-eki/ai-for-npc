35aa47a548fc7d3b8c6a003ac363c896e7c6138c
Louis Sanna <louis.sanna@ekimetrics.com>
2023-03-11 22:22:10 +0100

    uxui

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -5,15 +5,22 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     className?: string;
   };
 
-function Button({ children, className = '', ...props }: ButtonProps) {
-  return (
-    <button
-      className={`rounded-md font-bold bg-purple-600 px-4 py-2 text-sm text-white hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-400 ${className}`}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+  function Button({ children, className = '', disabled = false, ...props }: ButtonProps) {
+    const buttonStyle = disabled
+      ? 'bg-gray-400 text-gray-500 cursor-not-allowed'
+      : 'bg-purple-600 hover:bg-purple-500 focus:ring-2 focus:ring-purple-400';
+    
+    return (
+      <button
+        className={`rounded-md font-bold px-4 py-2 text-sm text-white focus:outline-none ${buttonStyle} ${className}`}
+        disabled={disabled}
+        aria-label={disabled ? 'This button is disabled' : undefined}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  };
+  
 
 export default Button;
