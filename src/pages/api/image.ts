f33f5b89630220f7d693d57d6f1d5bfcab3cd46c
Louis Sanna <louis.sanna@ekimetrics.com>
2023-03-12 17:08:17 +0100

    fae

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -1,23 +1,27 @@
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export default async function handler(req: Request) {
-    const { query } = (await req.json()) as { query?: string; }
-    const response = await fetch(`https://lexica.art/api/v1/search?q=${query}`);
-    const data = await response.json();
-    const firstImage = data.images[0]; // extract the first image object
-    return new Response(
-        JSON.stringify({
-            id: firstImage.id,
-            src: firstImage.src,
-            prompt: firstImage.prompt,
-            width: firstImage.width,
-            height: firstImage.height
-        }),
-        {
-          status: 200,
-          headers: {
-            'content-type': 'application/json',
-          },
-        }
-      )
+  const { query } = (await req.json()) as { query?: string };
+  const response = await fetch(`https://lexica.art/api/v1/search?q=${query}`, {
+    headers: { "Content-Type": "application/json" },
+    method: "GET",
+  });
+  console.log("response", response);
+  const data = await response.json();
+  const firstImage = data.images[0]; // extract the first image object
+  return new Response(
+    JSON.stringify({
+      id: firstImage.id,
+      src: firstImage.src,
+      prompt: firstImage.prompt,
+      width: firstImage.width,
+      height: firstImage.height,
+    }),
+    {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
 }
