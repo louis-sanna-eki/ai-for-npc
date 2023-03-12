46fa272bf2ca6ad0ca13a69def7d7e1823b14ce8
Louis Sanna <louis.sanna@ekimetrics.com>
2023-03-12 14:21:44 +0100

    Fix ui

--- src/pages/components/Dialog.tsx
+++ src/pages/components/Dialog.tsx
@@ -5,7 +5,9 @@ import { ChatGPTMessage } from "~/utils/OpenAIStream";
 import Button from "./Button";
 
 export default function Dialog({ character }: { character?: Character }) {
-  const prompt = character ?  buildPrompt(character?.data as unknown as Template) : "placeholder prompt";
+  const prompt = character
+    ? buildPrompt(character?.data as unknown as Template)
+    : "placeholder prompt";
   const [isLoading, setIsLoading] = useState(false);
   const [dialog, setDialog] = useState<string>("");
   const [messages, setMessages] = useState<ChatGPTMessage[]>([
@@ -34,16 +36,17 @@ export default function Dialog({ character }: { character?: Character }) {
     });
   }, [dialog, isLoading]);
 
-  const isReadyForGeneration = messages.slice(-1)[0]?.role === "user" && !isLoading;
+  const isReadyForGeneration =
+    messages.slice(-1)[0]?.role === "user" && !isLoading;
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4 w-full">
+    <div className="flex w-full flex-col items-center justify-center gap-4">
       <h2 className="text-2xl font-bold text-white">{`Speak with ${character?.name}!`}</h2>
-      <div className="my-2 space-y-10 w-full flex flex-col items-center justify-center">
+      <div className="my-2 flex w-full flex-col items-center justify-center space-y-10">
         <Messages messages={liveMessages} />
         <AddMessage
           onAdd={(message) => {
-            const newMessages = [...messages, message]
+            const newMessages = [...messages, message];
             setMessages(newMessages);
             generateDialog(newMessages);
           }}
@@ -53,7 +56,7 @@ export default function Dialog({ character }: { character?: Character }) {
   );
 
   async function generateDialog(currentMessages: ChatGPTMessage[]) {
-    console.log("currentMessages", currentMessages)
+    console.log("currentMessages", currentMessages);
     setDialog("");
     setIsLoading(true);
     const response = await fetch("/api/generate", {
@@ -91,11 +94,11 @@ function AddMessage({ onAdd }: { onAdd: (msg: ChatGPTMessage) => void }) {
   const addMessage = () => {
     onAdd({ role: "user", content });
     setContent("");
-  }
+  };
   return (
     <div className="flex h-12 gap-2">
       <input
-        className="rounded-md border-2 border-zinc-800 px-4 py-2 text-black focus:outline-none w-96"
+        className="w-96 rounded-md border-2 border-zinc-800 px-4 py-2 text-black focus:outline-none"
         placeholder="Have your say.."
         minLength={10}
         maxLength={400}
@@ -107,10 +110,7 @@ function AddMessage({ onAdd }: { onAdd: (msg: ChatGPTMessage) => void }) {
           }
         }}
       />
-      <Button
-        disabled={!content.trim()}
-        onClick={addMessage}
-      >
+      <Button disabled={!content.trim()} onClick={addMessage}>
         Add
       </Button>
     </div>
@@ -118,20 +118,37 @@ function AddMessage({ onAdd }: { onAdd: (msg: ChatGPTMessage) => void }) {
 }
 
 function Messages({ messages }: { messages: ChatGPTMessage[] }) {
-  console.log("messages", messages)
+  console.log("messages", messages);
   return (
     <div>
       {messages.slice(2).map(({ content, role }, index) => (
-        <TextBox key={index} className={role === "assistant"? "bg-purple-500 hover:bg-purple-400 text-white": ""}>{content}</TextBox>
+        <TextBox
+          key={index}
+          className={
+            role === "assistant"
+              ? "bg-purple-500 text-white hover:bg-purple-400"
+              : "bg-white text-black hover:bg-gray-100"
+          }
+        >
+          {content}
+        </TextBox>
       ))}
     </div>
   );
 }
 
-function TextBox({ children, className= "" }: { children: ReactNode; className?: string;}) {
+function TextBox({
+  children,
+  className = "",
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="mx-auto flex max-w-xl flex-col items-center justify-center space-y-8 mt-4">
-      <div className={`rounded-xl border bg-white p-4 text-black shadow-md transition hover:bg-gray-100 ${className}`}>
+    <div className="mx-auto mt-4 flex max-w-xl flex-col items-center justify-center space-y-8">
+      <div
+        className={`rounded-xl border  p-4 shadow-md transition  ${className}`}
+      >
         {children}
       </div>
     </div>
